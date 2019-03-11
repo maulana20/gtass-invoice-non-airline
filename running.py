c90912ff23edfbd831509a6ceb9f751ef799055c
maulana <maulanasaputra11091082@gmail.com>
2019-03-11 11:27:50 +0700

    info konsorsium name
    Signed-off-by: maulana <maulanasaputra11091082@gmail.com>

--- running.py
+++ running.py
@@ -3,6 +3,10 @@ import sys
 
 from models import PExcelModel
 
+def getKonsorsiumName(i):
+	konsorsium_list = ['KAI']
+	return konsorsium_list[i-1]
+
 print('**************************************')
 print('* Konsorsium yang di pilih           *')
 print('**************************************')
@@ -10,13 +14,20 @@ print('* 1. KAI                             *')
 print('**************************************')
 
 konsorsium_choice = input('> ')
+konsorsium_choice = int(konsorsium_choice)
 
 if not konsorsium_choice:
 	print('result : Konsorsium harus di pilih tidak boleh kosong !')
 	sys.exit()
 
-if konsorsium_choice not in ['1']:
+if konsorsium_choice not in [1]:
 	print('result : Konsorsium yang di pilih tidak ada !')
+	sys.exit()
+
+konsorsium_name = getKonsorsiumName(konsorsium_choice)
+
+if not konsorsium_name:
+	print('result : Konsorsium yang di pilih kosong !')
 	sys.exit()
 
 file_name = input('Nama File : \n')
@@ -29,7 +40,7 @@ file_dir = 'file/' + file_name
 
 is_file_exists = True if os.path.exists(file_dir) else False
 if is_file_exists == False:
-	print('result : Nama file tidak ada !')
+	print('result : Nama file (' + konsorsium_name + ') tidak ada !')
 	sys.exit()
 
 pexcel = PExcelModel(file_dir)
@@ -60,4 +71,4 @@ confirm = input('Simpan data ? (Y/N)\n')
 if confirm == 'Y' or confirm == 'y':
 	print('running')
 else :
-	print('not running')
+	sys.exit()
